b663a82ac45b78ab4679fcf7cb97c7eb5f828e37
unknown <yes.kanat@gmail.com>
2024-05-13 22:27:28 +0300

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,33 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 71, 160, 93));
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses()
-    ),
+    MaterialApp(
+        theme: ThemeData().copyWith(
+            colorScheme: kColorScheme,
+            appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer,
+            ),
+            cardTheme: CardTheme(
+              color: kColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer,
+              ),
+            ),
+            textTheme: ThemeData().textTheme.copyWith(
+                    titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 14,
+                ))),
+        home: const Expenses()),
   );
 }
